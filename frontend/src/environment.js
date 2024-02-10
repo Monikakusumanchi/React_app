410b39a7b6b173130a9a10d09d2049427246c4f5
Monika kusumanchi <kusumanchimonika1407@gmail.com>
2024-02-10 09:13:53 +0000

    env error resolved

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    fastAPIUrl: 'https://monikakusumanc-reactapp-zav1nb18alv.ws-us108.gitpod.io/get_gemini_completion'
+    fastAPIUrl: "https://8000-monikakusumanc-reactapp-zn729xfad4j.ws-us108.gitpod.io/get_gemini_completion"
   };
